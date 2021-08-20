0bb719895bbd1549ca7d775eb50b5a86156b9902
Tanasenko Konstantin <tanasenkok@gmail.com>
2021-08-20 13:45:33 +0400

    добавил балун

--- index.js
+++ index.js
@@ -5,24 +5,48 @@ ymaps.ready(init);
             zoom: 11,
             controls: []
         });
-    
-        var myCollection = new ymaps.GeoObjectCollection({}, {
-            preset: 'islands#redIcon', //все метки красные
-            draggable: false 
-        });
 
-        var coords = [];
+        var coords = {};
+        var dataStorage = localStorage.data;
+        var myBallon = {
+            balloonContent: [
+                '<div class="balloon">',
+                    '<div class="reviews"></div>',
+                    '<div class="title">Отзыв:</div>',
+                    '<input class="name" placeholder="Укажите ваше имя">',
+                    '<input class="place" placeholder="Укажите место">',
+                    '<textarea class="comment" placeholder="Оставить отзыв"></textarea>',
+                    '<button class="addButton">Добавить</button>',
+                '</div>'
+            ].join('')
+        };
+
+        var addButton = document.querySelector('.addButton');
+        var nameInput = document.querySelector('.name');
+        var placeInput = document.querySelector('.place');
+        var commentInput = document.querySelector('.comment');
+        
 
         myMap.events.add('click', function(e) {
             var coord = e.get('coords');
-            console.log(coord);
-            coords.push(coord);
-            console.log(coords);
+            coords.coord = coord;
+            myMap.geoObjects.add(new ymaps.Placemark((coord), myBallon));
+            addButton.addEventListener('click', function(){
+                var review = document.createElement('div');
+                review.classList.add = 'review';
+                review.innerHTML = `${nameInput.value}` + ' ' + `${placeInput.value}`  + ' ' + `${commentInput.value}`;
+                reviews.append(review);
+            });
+            dataStorage = JSON.stringify(coords);
+            console.log('Добавляем координаты ', dataStorage);
         })
-    
-        for (var i = 0; i < coords.length; i++) {
-            myCollection.add(new ymaps.Placemark(coords[i]));
-        }
-    
-        myMap.geoObjects.add(myCollection);
+
+        //загрузка точек из localStorage
+        /*window.addEventListener('load', function () {
+            var dataCoords = JSON.parse(dataStorage);
+            dataCoords.forEach(element => {
+                myMap.geoObjects.add(new ymaps.Placemark(element));
+            });
+            console.log('Добавляем плейсмарки ', dataCoords);
+        });*/
     }
